8334f8f937268ba094f96814e5488a4ce8bd9314
Nuno <1161042@isep.ipp.pt>
2022-01-11 21:37:06 +0000

    Sites load

--- Files/SQL/StagingArea/CreateStageTableSites_PlusOrders.sql
+++ Files/SQL/StagingArea/CreateStageTableSites_PlusOrders.sql
@@ -8,7 +8,6 @@ IF NOT EXISTS (SELECT name FROM sys.tables WHERE name = 'Site_PlusOrders')
 	[SiteName] [nvarchar](255) NULL,
 	[Initials] [nvarchar](3) NULL,
 	[Currency] [nvarchar](3) NULL,
-	[SiteInfoID] [int] NULL,
 	[StoreContact] [nvarchar](255) NULL,
 	[AddressLine1] [nvarchar](255) NULL,
 	[AddressLine2] [nvarchar](255) NULL,

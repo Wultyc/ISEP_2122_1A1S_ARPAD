059a4d736738da4c3449cc921d36c30b2859f75a
Nuno <1161042@isep.ipp.pt>
2022-01-14 22:47:39 +0000

    Separation of loading

--- Files/SQL/StagingArea/CreateStageTableCustomers_MoreOrders.sql
+++ Files/SQL/StagingArea/CreateStageTableCustomers_MoreOrders.sql
@@ -2,7 +2,7 @@ IF NOT EXISTS (SELECT name FROM sys.tables WHERE name = 'Customers_MoreOrders')
 	CREATE TABLE [dbo].[Customers_MoreOrders]
 	(
 		[CustomerID] [int] NOT NULL,
-		[CountryID] [int] NULL,
+		[CountryCode] [nvarchar](2) NULL,
 		[Gender] [nchar](1) NULL,
 		[GeographicRegion] [int] NULL,
 		[Language] [nvarchar](2) NULL,
--- Files/SQL/StagingArea/CreateStageTableCustomers_PlusOrders.sql
+++ Files/SQL/StagingArea/CreateStageTableCustomers_PlusOrders.sql
@@ -2,7 +2,7 @@ IF NOT EXISTS (SELECT name FROM sys.tables WHERE name = 'Customers_PlusOrders')
 	CREATE TABLE [dbo].[Customers_PlusOrders]
 	(
 		[CustomerID] [int] NOT NULL,
-		[CountryID] [int] NULL,
+		[CountryCode] [nvarchar](2) NULL,
 		[Gender] [nchar](1) NULL,
 		[GeographicRegion] [int] NULL,
 		[Language] [nvarchar](2) NULL,

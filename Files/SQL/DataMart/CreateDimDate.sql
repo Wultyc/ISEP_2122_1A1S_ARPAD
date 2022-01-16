ca8eac7b553eec4b80f0c7fa6b47672f5bf37e56
Nuno <1161042@isep.ipp.pt>
2022-01-16 14:52:44 +0000

    Data mart creation. Dim Date creation script.

---
+++ Files/SQL/DataMart/CreateDimDate.sql
@@ -0,0 +1,37 @@
+IF NOT EXISTS (SELECT name FROM sys.tables WHERE name = 'DimDate')	
+	CREATE TABLE [dbo].[DimDate](
+		[DateKey] [int] IDENTITY(1,1) NOT NULL,
+		[FullDate] [datetime] NOT NULL,
+		[DateName] [nvarchar] (100) NOT NULL,
+		[YearName] [nvarchar] (100) NOT NULL,
+		[MonthName] [nvarchar] (100) NOT NULL,
+		[WeekName] [nvarchar] (100) NOT NULL,
+		[DayOfYear] [int] NOT NULL,
+		[DayOfMonth] [int] NOT NULL,
+		[DayOfWeek] [int] NOT NULL,
+		[WeekOfYear] [int] NOT NULL,
+		[Year] [int] NOT NULL,
+		[Semester] [tinyint] NOT NULL,
+		[Quarter] [tinyint] NOT NULL,
+		[Month] [int] NOT NULL,
+		[MonthName] [nvarchar](10) NOT NULL,
+		[Week] [tinyint] NOT NULL,
+		[DayOfWeek] [nvarchar](10) NOT NULL,
+		[Weekend] [nvarchar](3) NOT NULL,
+		[Season] [nvarchar] (10) NOT NULL,
+		[FirstDayOfMonth] [nvarchar](3) NOT NULL,
+		[LastDayOfMonth] [nvarchar](3) NOT NULL,
+		[YearSemester] [tinyint] not null,
+		[YearSemesterName] [nvarchar] (100) not null,
+		[YearTrimester] [tinyint] not null,
+		[YearTrismesterName] [nvarchar] (100) not null,
+		[YearQuarter] [tinyint] not null,
+		[YearQuarterName] [nvarchar] (100) not null,
+		[LeapYear] [nvarchar] (3)
+
+
+	 CONSTRAINT [PK_DimDate] PRIMARY KEY CLUSTERED 
+	(
+		[DateKey] ASC
+	)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+	) ON [PRIMARY]

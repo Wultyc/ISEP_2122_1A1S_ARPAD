f63127f04cc5a61256dd6826b127b408f1ad0705
Nuno <1161042@isep.ipp.pt>
2022-01-16 15:59:13 +0000

    Dim Date

--- Files/SQL/DataMart/CreateDimDate.sql
+++ Files/SQL/DataMart/CreateDimDate.sql
@@ -4,32 +4,27 @@ IF NOT EXISTS (SELECT name FROM sys.tables WHERE name = 'DimDate')
 		[FullDate] [datetime] NOT NULL,
 		[DateName] [nvarchar] (100) NOT NULL,
 		[YearName] [nvarchar] (100) NOT NULL,
-		[MonthName] [nvarchar] (100) NOT NULL,
+		[MonthNameFull] [nvarchar] (100) NOT NULL,
 		[WeekName] [nvarchar] (100) NOT NULL,
 		[DayOfYear] [int] NOT NULL,
 		[DayOfMonth] [int] NOT NULL,
 		[DayOfWeek] [int] NOT NULL,
 		[WeekOfYear] [int] NOT NULL,
+		[QuarterOfYear][tinyint] not null,
+		[QuarterName] [nvarchar] (100) not null,
+		[TrimesterOfYear] [tinyint] not null,
+		[TrismesterName] [nvarchar] (100) not null,
+		[Half-YearOfYear] [tinyint] not null,
+		[Half-YearName] [nvarchar] (100) not null,
 		[Year] [int] NOT NULL,
-		[Semester] [tinyint] NOT NULL,
-		[Quarter] [tinyint] NOT NULL,
+		[LeapYear] [nvarchar] (3),
 		[Month] [int] NOT NULL,
 		[MonthName] [nvarchar](10) NOT NULL,
-		[Week] [tinyint] NOT NULL,
-		[DayOfWeek] [nvarchar](10) NOT NULL,
+		[DayOfWeekFull][nvarchar](10) NOT NULL,
 		[Weekend] [nvarchar](3) NOT NULL,
 		[Season] [nvarchar] (10) NOT NULL,
 		[FirstDayOfMonth] [nvarchar](3) NOT NULL,
-		[LastDayOfMonth] [nvarchar](3) NOT NULL,
-		[YearSemester] [tinyint] not null,
-		[YearSemesterName] [nvarchar] (100) not null,
-		[YearTrimester] [tinyint] not null,
-		[YearTrismesterName] [nvarchar] (100) not null,
-		[YearQuarter] [tinyint] not null,
-		[YearQuarterName] [nvarchar] (100) not null,
-		[LeapYear] [nvarchar] (3)
-
-
+		[LastDayOfMonth] [nvarchar](3) NOT NULL
 	 CONSTRAINT [PK_DimDate] PRIMARY KEY CLUSTERED 
 	(
 		[DateKey] ASC
